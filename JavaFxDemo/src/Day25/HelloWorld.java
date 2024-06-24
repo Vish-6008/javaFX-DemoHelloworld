ef9b9703bc5780741350442a38a02618b5362af8
DELL <DELL@DESKTOP-8S0EJGG>
2024-06-24 17:29:50 +0530

    Commit

---
+++ JavaFxDemo/src/Day25/HelloWorld.java
@@ -0,0 +1,52 @@
+/********************************************************************************************
+ *   COPYRIGHT (C) 2024 CREVAVI TECHNOLOGIES PVT LTD
+ *   The reproduction, transmission or use of this document/file or its
+ *   contents is not permitted without written authorization.
+ *   Offenders will be liable for damages. All rights reserved.
+ *---------------------------------------------------------------------------
+ *   Purpose:  Below java code print's the ,message when i click on the button with action mode.
+ *   Project: Hello-World Demo-code 
+ *   Platform: Cross-platform (Windows, macOS, Linux)
+ *   Compiler: JDK-22
+ *   IDE:      Eclipse IDE for Enterprise Java and Web Developers (includes Incubating components)
+ *	 Version: 2024-03 (4.31.0)
+ *   Build id: 20240307-1437
+ * ***********************************************************/
+
+
+package Day25;
+
+
+
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+ 
+public class HelloWorld extends Application {
+    public static void main(String[] args) {
+        launch(args);
+    }
+    
+    @Override
+    public void start(Stage primaryStage) {
+        primaryStage.setTitle("Hello World!");
+        Button btn = new Button();
+        btn.setText("Say 'Hello World'");
+        btn.setOnAction(new EventHandler<ActionEvent>() {
+ 
+            @Override
+            public void handle(ActionEvent event) {
+                System.out.println("Hello World!");
+            }
+        });
+        
+        StackPane root = new StackPane();
+        root.getChildren().add(btn);
+        primaryStage.setScene(new Scene(root, 300, 250));
+        primaryStage.show();
+    }
+}
